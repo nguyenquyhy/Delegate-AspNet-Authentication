f15f02ed6e58a7b4610c0000cc18fed31692d20d
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2020-08-05 17:43:13 +0800

    Add MVC endpoints

---
+++ AspNetAuthentication/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetAuthentication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetAuthentication/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public string Get()
+        {
+            return "Hello API!";
+        }
+
+        [Authorize]
+        [Route("Profile")]
+        public string Profile()
+        {
+            return "Profile";
+        }
+    }
+}
